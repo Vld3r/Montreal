7d0af27f1efeb1de29955c1bc269dacf183d5c77
Andrey <85394829+andreyoala@users.noreply.github.com>
2024-08-21 12:21:03 +0300

    vv2

--- lib/11urlsHJW.dart
+++ lib/11urlsHJW.dart
@@ -1,4 +1,4 @@
 class Url {
   static String privacy = 'https://www.termsfeed.com/live/181bcaa1-c3ec-444a-9010-fc10287f3059';
-  static String term = 'https://www.termsfeed.com/live/e33db7f7-6f89-404c-bf9f-95d0a7c5dd7d';
+  static String term = 'https://app.websitepolicies.com/policies/view/b6ty83lo#links-to-other-resources';
 }
